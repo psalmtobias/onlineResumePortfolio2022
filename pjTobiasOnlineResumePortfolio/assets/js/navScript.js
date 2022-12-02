a00fe154af3c172fe1af8e0c0f8514ad2fb23fcf
psalmtobias <98985017+psalmtobias@users.noreply.github.com>
2022-12-02 11:49:30 +0800

    removed

--- pjTobiasOnlineResumePortfolio/assets/js/navScript.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.getElementsByClassName('pj-toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('pj-navbar-links')[0]
-
-toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
-})
